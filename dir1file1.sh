1a2700d96b9143848b0f16c26acf3c54415c1d77
shivam <grshivam30@gmail.com>
2024-08-05 09:05:55 +0000

    Added content in branch

---
+++ dir1file1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+username=admin
+password=testyantra123
+
+echo "Enter the username "
+read un
+
+echo "Enter the password "
+read passwd
+
+if (( $un == $username)) && (( $passwd == $password))
+then
+	echo "Authetication successful"
+else
+	echo "Authentication unsuccessful"
+
+fi
